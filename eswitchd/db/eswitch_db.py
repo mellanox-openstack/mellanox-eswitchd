mellanox-openstack/mellanox-eswitchd

commit 231e9979a0e089ca8d266d0ab853f8c272c978ea

eswitchd pep8 fixes
Change-Id: I2c35e1f8190590dc3be598e0e81449be19ee87d8
Signed-off-by: Lenny Verkhovsky <lennyb@mellanox.com>

eswitchd/db/eswitch_db.py
@@ -20,170 +20,174 @@ from eswitchd.common import constants
 LOG = logging.getLogger(__name__)
 
 
-class eSwitchDB():
+class eSwitchDB(object):
 
-        def __init__(self):
-            self.port_table  = {}
-            self.port_policy = {}
+    def __init__(self):
+        self.port_table = {}
+        self.port_policy = {}
 
-        def create_port(self, port_name, port_type):
-            self.port_table.update({port_name: {'type': port_type,
-                                                'vnic': None,
-                                                'state': None,
-                                                'alias': None,
-                                                'device_id': None}})
+    def create_port(self, port_name, port_type):
+        self.port_table.update({port_name: {'type': port_type,
+                                            'vnic': None,
+                                            'state': None,
+                                            'alias': None,
+                                            'device_id': None}})
 
-        def plug_nic(self, port_name):
-            self.port_table[port_name]['state'] = constants.VPORT_STATE_ATTACHED
-            LOG.info("port table:", self.port_table)
+    def plug_nic(self, port_name):
+        self.port_table[port_name]['state'] = constants.VPORT_STATE_ATTACHED
+        LOG.info("port table:", self.port_table)
 
-        def get_ports(self):
-            return self.port_table
+    def get_ports(self):
+        return self.port_table
 
-        def get_port_type(self,dev):
+    def get_port_type(self, dev):
+        return self.port_table[dev]['type']
+
+    def get_port_state(self, dev):
+        state = None
+        dev = self.port_table.get(dev)
+        if dev:
+            state = dev.get('state')
+        return state
+
+    def get_attached_vnics(self):
+        vnics = {}
+        for port in self.port_table.values():
+            vnic_mac = port['vnic']
+            state = port['state']
+            device_id = self.port_policy[vnic_mac]['device_id']
+            if vnic_mac and state == constants.VPORT_STATE_ATTACHED:
+                vnics[vnic_mac] = {'mac': vnic_mac, 'device_id': device_id}
+        return vnics
+
+    def get_port_policy(self):
+        return self.port_policy
+
+    def get_port_policy_matrix(self):
+        table_matrix = [['VNIC_MAC', 'VLAN', 'DEV', 'DEVICE_ID']]
+        for vnic_mac, port_policy in self.port_policy.items():
+            table_matrix.append([vnic_mac, port_policy['vlan'],
+                                 port_policy['dev'], port_policy['device_id']])
+        return table_matrix
+
+    def get_port_table(self):
+        return self.port_table
+
+    def get_port_table_matrix(self):
+        table_matrix = [['PORT_NAME', 'TYPE', 'VNIC', 'STATE', 'ALIAS',
+                         'DEVICE_ID']]
+        for port_name, port_data in self.port_table.items():
+            table_matrix.append([port_name, port_data['type'],
+                                 port_data['vnic'],
+                                 port_data['state'], port_data['alias'],
+                                 port_data['device_id']])
+        return table_matrix
+
+    def get_vlan(self, vnic_mac):
+        if self.vnic_exists(vnic_mac):
+            return self.port_policy[vnic_mac]['vlan']
+        return
+
+    def create_vnic(self, vnic_mac):
+        if not self.vnic_exists(vnic_mac):
+            self.port_policy.update({vnic_mac: {'vlan': None,
+                                                'dev': None,
+                                                'device_id': None,
+                                                'port_id': None}})
+
+    def get_dev_type(self, dev):
+        dev_type = None
+
+        if dev in self.port_table:
+            dev_type = self.port_table[dev]['type']
+        return dev_type
+
+    def get_dev_alias_for_vnic(self, vnic_mac):
+        alias = None
+        dev = self.get_dev_for_vnic(vnic_mac)
+        if dev:
+            alias = self.port_table[dev].get('alias')
+        return alias
+
+    def get_dev_type_for_vnic(self, vnic_mac):
+        dev = None
+
+        if vnic_mac in self.port_policy:
+            if 'dev' in self.port_policy[vnic_mac]:
+                dev = self.port_policy[vnic_mac]['dev']
+        if dev:
             return self.port_table[dev]['type']
+        else:
+            return None
 
-        def get_port_state(self, dev):
-            state = None
-            dev = self.port_table.get(dev)
-            if dev:
-                state = dev.get('state')
-            return state
+        if vnic_mac in self.port_policy:
+            if 'dev' in self.port_policy[vnic_mac]:
+                dev = self.port_policy[vnic_mac]['dev']
+        return dev
 
-        def get_attached_vnics(self):
-            vnics = {}
-            for port in self.port_table.values():
-                vnic_mac = port['vnic']
-                state   = port['state']
-                if vnic_mac and state == constants.VPORT_STATE_ATTACHED:
-                    vnics[vnic_mac]={'mac': vnic_mac,
-                                     'device_id': self.port_policy[vnic_mac]['device_id']}
-            return vnics
+    def get_dev_for_vnic(self, vnic_mac):
+        dev = None
+        if vnic_mac in self.port_policy:
+            if 'dev' in self.port_policy[vnic_mac]:
+                dev = self.port_policy[vnic_mac]['dev']
+        return dev
 
-        def get_port_policy(self):
-            return self.port_policy
+    def get_vnic_state(self, vnic_mac):
+        dev_state = None
+        dev = self.get_dev_for_vnic(vnic_mac)
+        if dev:
+            dev_state = self.get_port_state(dev)
+        return dev_state
 
-        def get_port_policy_matrix(self):
-            table_matrix = [['VNIC_MAC','VLAN','DEV','DEVICE_ID']]
-            for vnic_mac, port_policy in self.port_policy.items():
-                table_matrix.append([vnic_mac,port_policy['vlan'], port_policy['dev'],port_policy['device_id']])
-            return table_matrix
-
-        def get_port_table(self):
-            return self.port_table
-
-        def get_port_table_matrix(self):
-            table_matrix = [['PORT_NAME','TYPE','VNIC','STATE','ALIAS','DEVICE_ID']]
-            for port_name, port_data in self.port_table.items():
-                table_matrix.append(
-                    [port_name, port_data['type'], port_data['vnic'], port_data['state'], port_data['alias'], port_data['device_id']])
-            return table_matrix
-
-        def get_vlan(self, vnic_mac):
-            if self.vnic_exists(vnic_mac):
-                return self.port_policy[vnic_mac]['vlan']
-            return
-
-        def create_vnic(self, vnic_mac):
-            if not self.vnic_exists(vnic_mac):
-                self.port_policy.update({vnic_mac: {'vlan': None,
-                                                    'dev': None,
-                                                    'device_id': None,
-                                                    'port_id': None}})
-
-        def get_dev_type(self, dev):
-            dev_type = None
-
-            if dev in self.port_table:
-                dev_type = self.port_table[dev]['type']
-            return dev_type
-
-        def get_dev_alias_for_vnic(self, vnic_mac):
-            alias = None
-            dev = self.get_dev_for_vnic(vnic_mac)
-            if dev:
-                alias = self.port_table[dev].get('alias')
-            return alias
-
-        def get_dev_type_for_vnic(self, vnic_mac):
-            dev = None
-
-            if vnic_mac in self.port_policy:
-                if 'dev' in self.port_policy[vnic_mac]:
-                    dev = self.port_policy[vnic_mac]['dev']
-            if dev:
-                return self.port_table[dev]['type']
-            else:
-                return None
-
-            if vnic_mac in self.port_policy:
-                if 'dev' in self.port_policy[vnic_mac]:
-                    dev = self.port_policy[vnic_mac]['dev']
-            return dev
-
-        def get_dev_for_vnic(self, vnic_mac):
-            dev = None
-            if vnic_mac in self.port_policy:
-                if 'dev' in self.port_policy[vnic_mac]:
-                    dev = self.port_policy[vnic_mac]['dev']
-            return dev
-
-        def get_vnic_state(self, vnic_mac):
-            dev_state = None
-            dev = self.get_dev_for_vnic(vnic_mac)
-            if dev:
-                dev_state = self.get_port_state(dev)
-            return dev_state
-
-        def vnic_exists(self, vnic_mac):
-            if vnic_mac in self.port_policy:
-                return True
-            else:
-                return False
-
-        def attach_vnic(self, port_name, device_id, vnic_mac,dev_name = None):
-            self.port_table[port_name]['vnic'] = vnic_mac
-            self.port_table[port_name]['alias'] = dev_name
-            self.port_table[port_name]['state'] = constants.VPORT_STATE_PENDING
-            self.port_table[port_name]['device_id'] = device_id
-            dev = self.get_dev_for_vnic(vnic_mac)
-            if not dev and vnic_mac != constants.INVALID_MAC:
-                if vnic_mac in self.port_policy:
-                    vnic_mac_entry = self.port_policy[vnic_mac]
-                    vnic_mac_entry['dev'] = port_name
-                    vnic_mac_entry['device_id'] = device_id
-                    vnic_mac_entry.setdefault('vlan', None)
-                else:
-                    self.port_policy.update({vnic_mac: {'vlan': None,
-                                                        'dev': port_name,
-                                                        'device_id': device_id,
-                                                        }})
-                return True
+    def vnic_exists(self, vnic_mac):
+        if vnic_mac in self.port_policy:
+            return True
+        else:
             return False
 
-        def detach_vnic(self, vnic_mac):
+    def attach_vnic(self, port_name, device_id, vnic_mac, dev_name=None):
+        self.port_table[port_name]['vnic'] = vnic_mac
+        self.port_table[port_name]['alias'] = dev_name
+        self.port_table[port_name]['state'] = constants.VPORT_STATE_PENDING
+        self.port_table[port_name]['device_id'] = device_id
+        dev = self.get_dev_for_vnic(vnic_mac)
+        if not dev and vnic_mac != constants.INVALID_MAC:
+            if vnic_mac in self.port_policy:
+                vnic_mac_entry = self.port_policy[vnic_mac]
+                vnic_mac_entry['dev'] = port_name
+                vnic_mac_entry['device_id'] = device_id
+                vnic_mac_entry.setdefault('vlan', None)
+            else:
+                self.port_policy.update({vnic_mac: {'vlan': None,
+                                                    'dev': port_name,
+                                                    'device_id': device_id,
+                                                    }})
+            return True
+        return False
+
+    def detach_vnic(self, vnic_mac):
+        dev = self.get_dev_for_vnic(vnic_mac)
+        if dev:
+            self.port_table[dev]['vnic'] = None
+            self.port_table[dev]['alias'] = None
+            self.port_table[dev]['state'] = constants.VPORT_STATE_UNPLUGGED
+            self.port_table[dev]['device_id'] = None
+        return dev
+
+    def port_release(self, vnic_mac):
+        try:
             dev = self.get_dev_for_vnic(vnic_mac)
-            if dev:
-                self.port_table[dev]['vnic'] = None
-                self.port_table[dev]['alias'] = None
-                self.port_table[dev]['state'] = constants.VPORT_STATE_UNPLUGGED
-                self.port_table[dev]['device_id'] = None
-            return dev
+            vnic = self.port_policy.pop(vnic_mac)
+            self.port_table[dev]['state'] = None
+            vnic['type'] = self.port_table[vnic['dev']]['type']
+            return vnic
+        except KeyError:
+            return
+        except IndexError:
+            return
 
-        def port_release(self, vnic_mac):
-            try:
-                dev = self.get_dev_for_vnic(vnic_mac)
-                vnic = self.port_policy.pop(vnic_mac)
-                self.port_table[dev]['state'] = None
-                vnic['type'] = self.port_table[vnic['dev']]['type']
-                return vnic
-            except KeyError:
-                return
-            except IndexError:
-                return
+    def set_vlan(self, vnic_mac, vlan):
+        if not self.vnic_exists(vnic_mac):
+            self.create_vnic(vnic_mac)
 
-        def set_vlan(self, vnic_mac, vlan):
-            if not self.vnic_exists(vnic_mac):
-                self.create_vnic(vnic_mac)
-
-            self.port_policy[vnic_mac]['vlan'] = vlan
+        self.port_policy[vnic_mac]['vlan'] = vlan

eswitchd/cli/conn_utils.py
@@ -19,8 +19,6 @@ from oslo_config import cfg
 import zmq
 
 from eswitchd.cli import exceptions
-from eswitchd.common import config
-from eswitchd.common import constants
 from eswitchd.utils.helper_utils import set_conn_url
 
 REQUEST_TIMEOUT = 50000
@@ -35,7 +33,7 @@ class ConnUtil(object):
         addr = cfg.CONF.DAEMON.socket_os_addr
         self.conn_url = set_conn_url(transport, addr, port)
 
-    def send_msg(self,msg):
+    def send_msg(self, msg):
         context = zmq.Context()
         socket = context.socket(zmq.REQ)
         socket.setsockopt(zmq.LINGER, 0)
@@ -52,7 +50,7 @@ class ConnUtil(object):
                     response = self.parse_response_msg(response_msg)
                     return response
             else:
-               print 'no result received'
+                print ('no result received')
         finally:
             socket.close()
             context.term()
@@ -65,12 +63,13 @@ class ConnUtil(object):
                 return msg['response']
             return
         elif msg['status'] == 'FAIL':
-            error_msg = "Action  %s failed: %s" %(msg['action'], msg['reason'])
+            error_msg = "Action %s failed: %s" % (msg['action'], msg['reason'])
         else:
             error_msg = "Unknown operation status %s" % msg['status']
         raise exceptions.MlxException(error_msg)
 
-    def allocate_nic(self, vnic_mac, device_id, fabric, vnic_type, dev_name=None):
+    def allocate_nic(self, vnic_mac, device_id, fabric, vnic_type,
+                     dev_name=None):
         msg = json.dumps({'action': 'create_port',
                           'vnic_mac': vnic_mac,
                           'device_id': device_id,
@@ -92,7 +91,6 @@ class ConnUtil(object):
         recv_msg = self.send_msg(msg)
         dev = recv_msg['dev']
         return dev
-
 
     def deallocate_nic(self, vnic_mac, fabric):
         msg = json.dumps({'action': 'delete_port',

eswitchd/cli/ebr_dbg.py
@@ -1,19 +1,38 @@
 #!/usr/bin/python
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2013 Mellanox Technologies, Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import sys
+
 from eswitchd.cli import conn_utils
 from eswitchd.cli import exceptions
-#from eswitchd.common import constants
 
 action = sys.argv[1]
 client = conn_utils.ConnUtil()
 
-def pprint_table(out,table):
+
+def pprint_table(out, table):
     """Prints out a table of data, padded for alignment
+
     @param out: Output stream (file-like object)
     @param table: The table to print. A list of lists.
-    Each row must have the same number of columns. """
-    
+    Each row must have the same number of columns.
+    """
+
     def get_max_width(table, index):
         """Get the maximum width of the given column index"""
         return max([len(str(row[index])) for row in table])
@@ -31,26 +50,25 @@ def pprint_table(out,table):
             col = str(row[i]).rjust(col_paddings[i] + 2)
             print >> out, col,
         print >> out
-   
-    
+
+
 def main():
     if action == 'get-tables':
         fabric = sys.argv[2]
         try:
             result = client.get_tables(fabric)
             for fabric, tables in result.items():
-                print "FABRIC = %s" % fabric
-                print"========================"
+                print ("FABRIC = %s" % fabric)
+                print ("========================")
                 for table, data in tables.items():
-                    print "TABLE: %s" % table
+                    print ("TABLE: %s" % table)
                     pprint_table(sys.stdout, data)
-                    print"========================"
+                    print ("========================")
         except exceptions.MlxException as e:
             sys.stderr.write("Error in get-tables command")
             sys.stderr.write(e.message)
             sys.exit(1)
-        # sys.stdout.write(str(result))
         sys.exit(0)
-        
+
 if __name__ == '__main__':
     main()

eswitchd/common/config.py
@@ -33,7 +33,8 @@ mlx_daemon_opts = [
                help=('Default timeout waiting for messages')),
     cfg.IntOpt('max_polling_count',
                default=5,
-               help=('Daemon will do sync after max_polling_count * default_timeout')),
+               help=('Daemon will do sync after max_polling_count '
+                     '* default_timeout')),
     cfg.StrOpt('rootwrap_conf',
                default='/etc/eswitchd/rootwrap.conf',
                help=('eswitchd rootwrap configuration file'))

eswitchd/common/constants.py
@@ -32,9 +32,9 @@ PKEY_INDEX_PATH = "/sys/class/infiniband/%s/iov/%s/ports/%s/pkey_idx/%s"
 
 # CX4
 CX4_GUID_NODE_PATH = ('/sys/class/infiniband/%(module)s/device/sriov/'
-                        '%(vf_num)s/node')
+                      '%(vf_num)s/node')
 CX4_GUID_PORT_PATH = ('/sys/class/infiniband/%(module)s/device/sriov/'
-                        '%(vf_num)s/port')
+                      '%(vf_num)s/port')
 CX4_GUID_POLICY_PATH = ('/sys/class/infiniband/%(module)s/device/sriov/'
                         '%(vf_num)s/policy')
 

eswitchd/eswitch_daemon.py
@@ -19,13 +19,14 @@
 import json
 import sys
 import zmq
+
 from oslo_config import cfg
 from oslo_log import log as logging
+
 from common import config
 from eswitch_handler import eSwitchHandler
-from utils.helper_utils import set_conn_url
 import msg_handler as message
-
+from utils.helper_utils import set_conn_url
 
 LOG = logging.getLogger(__name__)
 
@@ -51,7 +52,7 @@ class MlxEswitchDaemon(object):
                     fabrics.append((fabric, pf))
                 except ValueError as ex:
                     LOG.error(_("Invalid fabric: "
-                                "'%(entry)s' - ",
+                                "'%(entry)s' - "
                                 "Service terminated!"),
                               locals())
                     raise
@@ -95,10 +96,11 @@ class MlxEswitchDaemon(object):
                 LOG.exception("exception during message handling - %s", e)
             if polling_counter == self.max_polling_count:
                 LOG.debug("Resync devices")
-            #    self.eswitch_handler.sync_devices()
+            # self.eswitch_handler.sync_devices()
                 polling_counter = 0
             else:
-                polling_counter+=1
+                polling_counter += 1
+
 
 def main():
     config.init(sys.argv[1:])
@@ -106,13 +108,13 @@ def main():
     try:
         daemon = MlxEswitchDaemon()
         daemon.start()
-    except Exception,e:
-        LOG.exception("Failed to start EswitchDaemon - Daemon terminated! %s",e)
+    except Exception as e:
+        LOG.exception("Failed to start EswitchDaemon - Daemon terminated! %s",
+                      e)
         sys.exit(1)
 
     daemon.daemon_loop()
 
+
 if __name__ == '__main__':
     main()
-
-

eswitchd/resource_mngr.py
@@ -13,23 +13,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import glob
 from lxml import etree
-import os
 
 import libvirt
 from oslo_log import log as logging
 
-
 from common import constants
-from common import exceptions
 from db import device_db
 from utils import pci_utils
 
 LOG = logging.getLogger(__name__)
 
 
-class ResourceManager:
+class ResourceManager(object):
 
     def __init__(self):
         self.pci_utils = pci_utils.pciUtils()
@@ -37,7 +33,8 @@ class ResourceManager:
 
     def add_fabric(self, fabric, pf, fabric_type):
         pci_id, hca_port, pf_mlx_dev = self._get_pf_details(pf)
-        self.device_db.add_fabric(fabric, pf, pci_id, hca_port, fabric_type, pf_mlx_dev)
+        self.device_db.add_fabric(fabric, pf, pci_id, hca_port, fabric_type,
+                                  pf_mlx_dev)
         vfs = self.discover_devices(pf)
         LOG.info("PF %s, vfs = %s" % (pf, vfs))
         self.device_db.set_fabric_devices(fabric, vfs)
@@ -63,14 +60,14 @@ class ResourceManager:
         for domain in domains:
             raw_xml = domain.XMLDesc(0)
             tree = etree.XML(raw_xml)
-            hostdevs   = tree.xpath("devices/hostdev/source/address")
+            hostdevs = tree.xpath("devices/hostdev/source/address")
             vm_id = tree.find('uuid').text
             for dev in self._get_attached_hostdevs(hostdevs):
                 devices.append(dev)
                 vm_ids[dev[0]] = vm_id
         return devices, vm_ids
 
-    def get_fabric_pf(self,fabric):
+    def get_fabric_pf(self, fabric):
         return self.device_db.get_pf(fabric)
 
     def get_fabric_details(self, fabric):
@@ -88,9 +85,10 @@ class ResourceManager:
         for fabric in fabrics:
             fabric_details = self.device_db.get_fabric_details(fabric)
             try:
-                macs_map[fabric] = self.pci_utils.get_vfs_macs_ib(fabric_details)
+                macs_map[fabric] = \
+                    self.pci_utils.get_vfs_macs_ib(fabric_details)
             except Exception:
-                LOG.exception("Failed to get vfs macs for fabric %s ",fabric)
+                LOG.exception("Failed to get vfs macs for fabric %s ", fabric)
                 continue
         return macs_map
 
@@ -101,24 +99,28 @@ class ResourceManager:
             fabric = self.get_fabric_for_dev(dev)
             if fabric:
                 fabric_details = self.get_fabric_details(fabric)
-                if fabric_details['pf_device_type'] == constants.CX3_VF_DEVICE_TYPE:
+                if fabric_details['pf_device_type'] == \
+                   constants.CX3_VF_DEVICE_TYPE:
                     hca_port = fabric_details['hca_port']
                     pf_mlx_dev = fabric_details['pf_mlx_dev']
-                    vf_index = self.pci_utils.get_guid_index(pf_mlx_dev, dev, hca_port)
-                elif fabric_details['pf_device_type'] == constants.CX4_VF_DEVICE_TYPE:
+                    vf_index = self.pci_utils.get_guid_index(pf_mlx_dev, dev,
+                                                             hca_port)
+                elif fabric_details['pf_device_type'] == \
+                    constants.CX4_VF_DEVICE_TYPE:
                     vf_index = fabric_details['vfs'][dev]['vf_num']
                 try:
                     mac = self.macs_map[fabric][str(vf_index)]
                     devs.append((dev, mac, fabric))
                 except KeyError:
-                    LOG.warning("Failed to retrieve Hostdev MAC for dev %s", dev)
+                    LOG.warning("Failed to retrieve Hostdev MAC for dev %s",
+                                dev)
             else:
                 LOG.info("No Fabric defined for device %s", hostdev)
         return devs
 
     def _get_pf_details(self, pf):
         hca_port = self.pci_utils.get_eth_port(pf)
-        pci_id  = self.pci_utils.get_pf_pci(pf)
-        pf_pci_id  = self.pci_utils.get_pf_pci(pf, 'normal')
+        pci_id = self.pci_utils.get_pf_pci(pf)
+        pf_pci_id = self.pci_utils.get_pf_pci(pf, 'normal')
         pf_mlx_dev = self.pci_utils.get_pf_mlx_dev(pf_pci_id)
         return (pci_id, hca_port, pf_mlx_dev)

eswitchd/tests/unit/test_pci_utils.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2013 Mellanox Technologies, Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import contextlib
 import mock
 import sys
@@ -5,6 +23,7 @@ sys.modules['ethtool'] = mock.Mock()
 
 from eswitchd.tests import base
 from eswitchd.utils import pci_utils
+
 
 class TestPciUtils(base.TestCase):
 

commit 099618b5f175d6e653350ced201cc175d9ba6601

Fixing eswitchd/db/eswitch_db.py caused by 231e9979a0e089ca8d266d0ab853f8c272c978ea
Change-Id: Ie01eba27c9b84b6f2cb3a7d029a502a6c0ca00f7
Signed-off-by: Lenny Verkhovsky <lennyb@mellanox.com>

eswitchd/db/eswitch_db.py
@@ -55,8 +55,8 @@ class eSwitchDB(object):
         for port in self.port_table.values():
             vnic_mac = port['vnic']
             state = port['state']
-            device_id = self.port_policy[vnic_mac]['device_id']
             if vnic_mac and state == constants.VPORT_STATE_ATTACHED:
+                device_id = self.port_policy[vnic_mac]['device_id']
                 vnics[vnic_mac] = {'mac': vnic_mac, 'device_id': device_id}
         return vnics
 